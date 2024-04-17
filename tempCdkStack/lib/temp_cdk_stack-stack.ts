2de2ef62e88e578bb080496f8a4f891743ece0a7
Armen Babakanian <armen.babakanian@gmail.com>
2024-04-17 12:19:42 -0700

    section 3.2

---
+++ tempCdkStack/lib/temp_cdk_stack-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+export class TempCdkStackStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    new s3.Bucket(this, "MyFirstBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: true,
+    });
+  }
+}
