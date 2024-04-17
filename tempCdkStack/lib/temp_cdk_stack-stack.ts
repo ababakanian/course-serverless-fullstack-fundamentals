87f34f7f24ea14cd5d3c65e8b446b5a2384ee260
Armen Babakanian <armen.babakanian@gmail.com>
2024-04-17 12:42:03 -0700

    section 3.6

--- tempCdkStack/lib/temp_cdk_stack-stack.ts
+++ tempCdkStack/lib/temp_cdk_stack-stack.ts
@@ -2,15 +2,20 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class TempCdkStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambdaNodeJs.NodejsFunction(this, "thisOfDay", {
+    const lambdaFunc = new lambdaNodeJs.NodejsFunction(this, "thisOfDay", {
       entry: "./lambda/timeOfDay.js",
       handler: "index",
       runtime: lambda.Runtime.NODEJS_20_X,
     });
+
+    const restApi = new apigateway.RestApi(this, "timeOfDayRestApi");
+
+    restApi.root.addMethod("GET", new apigateway.LambdaIntegration(lambdaFunc));
   }
 }
