e4865066ad1fe6070d72b5fc907c573fb76a5c59
Armen Babakanian <armen.babakanian@gmail.com>
2024-04-17 12:38:53 -0700

    section 3.5

--- tempCdkStack/lib/temp_cdk_stack-stack.ts
+++ tempCdkStack/lib/temp_cdk_stack-stack.ts
@@ -1,42 +1,16 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as s3 from "aws-cdk-lib/aws-s3";
-
-class ImageGallery extends Construct {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id);
-
-    // example resource
-    new s3.Bucket(this, "OriginalSizeImagesBucket", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      versioned: true,
-    });
-
-    new s3.Bucket(this, "ThumbnailSizeImagesBucket", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      versioned: true,
-    });
-  }
-}
-
-class PhotoManagement extends Construct {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id);
-    new ImageGallery(this, "photoAlbumGallery");
-  }
-}
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class TempCdkStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // create two image galleries
-    // new ImageGallery(this, "firstCustomStorage");
-    // new ImageGallery(this, "secondCustomStorage");
-
-    // create one photo management construct
-    new PhotoManagement(this, "photoManagement");
+    new lambdaNodeJs.NodejsFunction(this, "thisOfDay", {
+      entry: "./lambda/timeOfDay.js",
+      handler: "index",
+      runtime: lambda.Runtime.NODEJS_20_X,
+    });
   }
 }
