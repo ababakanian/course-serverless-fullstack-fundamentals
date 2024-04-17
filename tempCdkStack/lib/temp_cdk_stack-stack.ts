0991c0c793104534ef1c9ec376c9067fea5bc754
Armen Babakanian <armen.babakanian@gmail.com>
2024-04-17 12:30:08 -0700

    section 3.4

--- tempCdkStack/lib/temp_cdk_stack-stack.ts
+++ tempCdkStack/lib/temp_cdk_stack-stack.ts
@@ -2,16 +2,41 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 
-export class TempCdkStackStack extends cdk.Stack {
+class ImageGallery extends Construct {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id);
 
-    // The code that defines your stack goes here
-
-    new s3.Bucket(this, "MyFirstBucket", {
+    // example resource
+    new s3.Bucket(this, "OriginalSizeImagesBucket", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       versioned: true,
     });
+
+    new s3.Bucket(this, "ThumbnailSizeImagesBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: true,
+    });
+  }
+}
+
+class PhotoManagement extends Construct {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id);
+    new ImageGallery(this, "photoAlbumGallery");
+  }
+}
+
+export class TempCdkStackStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // create two image galleries
+    // new ImageGallery(this, "firstCustomStorage");
+    // new ImageGallery(this, "secondCustomStorage");
+
+    // create one photo management construct
+    new PhotoManagement(this, "photoManagement");
   }
 }
